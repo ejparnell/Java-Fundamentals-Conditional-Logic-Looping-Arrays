4b3c72ed6e6182ad95c2e15a94c9a6a99acfe05d
ejparnell <ejparnell@hotmail.com>
2019-09-12 15:22:59 -0400

    Used string builder to build stings

--- src/com/pluralsight/calcEngine/Main.java
+++ src/com/pluralsight/calcEngine/Main.java
@@ -50,5 +50,18 @@ public class Main {
 		    System.out.print("results = ");
 		    System.out.println(calculator.getResult());
         }
+
+		String[] statements = {
+		        "divide 100.0 50.0",
+                "add 25.0 92.0",
+                "subtract 225.0 17.0",
+                "multiply 11.0 3.0"
+        };
+
+		CalculateHelper helper = new CalculateHelper();
+		for (String statement:statements) {
+		    helper.process(statement);
+		    System.out.println(helper);
+        }
     }
 }
---
+++ src/com/pluralsight/calcEngine/MathCommand.java
@@ -0,0 +1,8 @@
+package com.pluralsight.calcEngine;
+
+public enum MathCommand {
+    Add,
+    Subtract,
+    Multiply,
+    Divide
+}
